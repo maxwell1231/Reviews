c9603cd0d5736483be2a05a481625b6c13e16d1b
Max Vaysburd <max.vays@gmail.com>
2019-04-13 17:38:31 -0400

    Implement extraction of word vectors from text file

--- idToVec.py
+++ idToVec.py
@@ -1,10 +1,24 @@
 import tensorflow as tf
 import numpy as np
 
+# with tf.Session() as sess:
+#     testArray = np.zeros((5,5))
+#     testVariable = tf.get_variable("test", initializer=testArray)
+#     tf.initializers.global_variables().run()
+#     print(testVariable.eval())
+#     saver = tf.train.Saver()
+#     saver.save(sess, "./my-model")
+
 with tf.Session() as sess:
-    testArray = np.zeros((5,5))
-    testVariable = tf.get_variable("test", initializer=testArray)
+    file = open("glove30k.txt")
+    counter = 0
+    arrayOfWordVecs = np.empty((0, 300))
     tf.initializers.global_variables().run()
-    print(testVariable.eval())
     saver = tf.train.Saver()
+    for line in file.readlines():
+        wordVec = line.split()
+        arrayOfWordVecs = np.append(arrayOfWordVecs, np.array([wordVec[1:]]), axis = 0)
+        counter += 1
+        print(counter)
+    wordVecVariable = tf.get_variable("wordVecs", initializer=arrayOfWordVecs)
     saver.save(sess, "./my-model")
